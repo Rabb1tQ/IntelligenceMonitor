5cb0ea2cab9eed4d0d0c0f68be464b5d88381036
Rabb1tQ <15024835320@163.com>
2024-07-17 01:04:44 +0800

    修改数据库脚本

--- sql/monitor.sql
+++ sql/monitor.sql
@@ -21,87 +21,114 @@ SET FOREIGN_KEY_CHECKS = 0;
 -- Table structure for avd_vul_info
 -- ----------------------------
 DROP TABLE IF EXISTS `avd_vul_info`;
-CREATE TABLE `avd_vul_info`  (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `vul_name` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT NULL,
-  `vul_type` varchar(100) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT NULL,
-  `vul_date` varchar(100) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT NULL,
-  `cve_number` varchar(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT NULL,
-  `exp_status` varchar(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT NULL,
-  `description` text CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL,
-  `suggest` text CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL,
-  `reference` varchar(500) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT NULL,
-  `level` varchar(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT NULL,
-  PRIMARY KEY (`id`) USING BTREE
-) ENGINE = MyISAM AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_unicode_ci ROW_FORMAT = Dynamic;
+CREATE TABLE `avd_vul_info`
+(
+    `id`          int(11)                                                 NOT NULL AUTO_INCREMENT,
+    `vul_name`    varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT NULL,
+    `vul_type`    varchar(100) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT NULL,
+    `vul_date`    varchar(100) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT NULL,
+    `cve_number`  varchar(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci  NULL DEFAULT NULL,
+    `exp_status`  varchar(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci  NULL DEFAULT NULL,
+    `description` text CHARACTER SET utf8 COLLATE utf8_unicode_ci         NULL,
+    `suggest`     text CHARACTER SET utf8 COLLATE utf8_unicode_ci         NULL,
+    `reference`   varchar(500) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT NULL,
+    `level`       varchar(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci  NULL DEFAULT NULL,
+    PRIMARY KEY (`id`) USING BTREE
+) ENGINE = MyISAM
+  AUTO_INCREMENT = 1
+  CHARACTER SET = utf8
+  COLLATE = utf8_unicode_ci
+  ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for cisa_vul_info
 -- ----------------------------
 DROP TABLE IF EXISTS `cisa_vul_info`;
-CREATE TABLE `cisa_vul_info`  (
-  `cve_number` varchar(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT NULL,
-  `vendor` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT NULL COMMENT '供应商',
-  `product` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT NULL,
-  `vulnerability_name` varchar(500) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT NULL,
-  `date_added` varchar(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT NULL COMMENT '漏洞公开日期',
-  `description` text CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL,
-  `suggest` text CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL COMMENT '缓解措施',
-  `reference` varchar(1000) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT NULL COMMENT '引用'
-) ENGINE = MyISAM CHARACTER SET = utf8 COLLATE = utf8_unicode_ci ROW_FORMAT = Dynamic;
+CREATE TABLE `cisa_vul_info`
+(
+    `cve_number`         varchar(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci   NULL DEFAULT NULL,
+    `vendor`             varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci  NULL DEFAULT NULL COMMENT '供应商',
+    `product`            varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci  NULL DEFAULT NULL,
+    `vulnerability_name` varchar(500) CHARACTER SET utf8 COLLATE utf8_unicode_ci  NULL DEFAULT NULL,
+    `date_added`         varchar(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci   NULL DEFAULT NULL COMMENT '漏洞公开日期',
+    `description`        text CHARACTER SET utf8 COLLATE utf8_unicode_ci          NULL,
+    `suggest`            text CHARACTER SET utf8 COLLATE utf8_unicode_ci          NULL COMMENT '缓解措施',
+    `reference`          varchar(1000) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT NULL COMMENT '引用'
+) ENGINE = MyISAM
+  CHARACTER SET = utf8
+  COLLATE = utf8_unicode_ci
+  ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for ms_vul_info
 -- ----------------------------
 DROP TABLE IF EXISTS `ms_vul_info`;
-CREATE TABLE `ms_vul_info`  (
-  `cve_number` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-  `release_date` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-  `mitre_url` varchar(500) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-  `tag` varchar(500) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-  PRIMARY KEY (`cve_number`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
+CREATE TABLE `ms_vul_info`
+(
+    `cve_number`   varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci  NOT NULL,
+    `release_date` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+    `mitre_url`    varchar(500) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+    `tag`          varchar(500) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+    PRIMARY KEY (`cve_number`) USING BTREE
+) ENGINE = InnoDB
+  CHARACTER SET = utf8mb4
+  COLLATE = utf8mb4_unicode_ci
+  ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for oscs_vul_info
 -- ----------------------------
 DROP TABLE IF EXISTS `oscs_vul_info`;
-CREATE TABLE `oscs_vul_info`  (
-  `public_time` varchar(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT NULL COMMENT '公开时间',
-  `title` varchar(500) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT NULL COMMENT '漏洞名称',
-  `level` varchar(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT NULL COMMENT '风险等级',
-  `description` text CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL COMMENT '漏洞描述',
-  `suggest` text CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL COMMENT '修复方案',
-  `reference` text CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL COMMENT '参考链接',
-  `vuln_type` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT NULL COMMENT '漏洞类型',
-  `cve_id` varchar(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT NULL,
-  `cnvd_id` varchar(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT NULL
-) ENGINE = MyISAM CHARACTER SET = utf8 COLLATE = utf8_unicode_ci ROW_FORMAT = Dynamic;
+CREATE TABLE `oscs_vul_info`
+(
+    `public_time` varchar(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci  NULL DEFAULT NULL COMMENT '公开时间',
+    `title`       varchar(500) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT NULL COMMENT '漏洞名称',
+    `level`       varchar(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci  NULL DEFAULT NULL COMMENT '风险等级',
+    `description` text CHARACTER SET utf8 COLLATE utf8_unicode_ci         NULL COMMENT '漏洞描述',
+    `suggest`     text CHARACTER SET utf8 COLLATE utf8_unicode_ci         NULL COMMENT '修复方案',
+    `reference`   text CHARACTER SET utf8 COLLATE utf8_unicode_ci         NULL COMMENT '参考链接',
+    `vuln_type`   varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT NULL COMMENT '漏洞类型',
+    `cve_id`      varchar(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci  NULL DEFAULT NULL,
+    `cnvd_id`     varchar(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci  NULL DEFAULT NULL
+) ENGINE = MyISAM
+  CHARACTER SET = utf8
+  COLLATE = utf8_unicode_ci
+  ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for repository
 -- ----------------------------
 DROP TABLE IF EXISTS `repository`;
-CREATE TABLE `repository`  (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `repo_name` varchar(1000) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '0',
-  `pushed_at` varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '',
-  `description` varchar(2000) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
-  PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 203 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+CREATE TABLE `repository`
+(
+    `id`          int(11)                                                        NOT NULL AUTO_INCREMENT,
+    `repo_name`   varchar(1000) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '0',
+    `pushed_at`   varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci  NOT NULL DEFAULT '',
+    `description` varchar(2000) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL     DEFAULT NULL,
+    PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB
+  AUTO_INCREMENT = 203
+  CHARACTER SET = utf8mb4
+  COLLATE = utf8mb4_general_ci
+  ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for ti_vul_info
 -- ----------------------------
 DROP TABLE IF EXISTS `ti_vul_info`;
-CREATE TABLE `ti_vul_info`  (
-  `vul_name` varchar(500) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL COMMENT '漏洞名称',
-  `description` text CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL COMMENT '描述',
-  `severity` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT NULL COMMENT '漏洞级别',
-  `publish_time` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT NULL COMMENT '发布时间',
-  `reference` text CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL COMMENT '引用',
-  `tags` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT NULL COMMENT '漏洞标签',
-  `vul_type` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT NULL COMMENT '漏洞类型'
-) ENGINE = MyISAM CHARACTER SET = utf8 COLLATE = utf8_unicode_ci ROW_FORMAT = DYNAMIC;
+CREATE TABLE `ti_vul_info`
+(
+    `vul_name`     varchar(500) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL COMMENT '漏洞名称',
+    `description`  text CHARACTER SET utf8 COLLATE utf8_unicode_ci         NULL COMMENT '描述',
+    `rating_level` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT NULL COMMENT '漏洞级别',
+    `publish_time` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT NULL COMMENT '发布时间',
+    `tags`         varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT NULL COMMENT '漏洞标签',
+    `vul_type`     varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT NULL COMMENT '漏洞类型 threat_category',
+    `cve_id`       varchar(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci  NULL DEFAULT NULL,
+    `cnvd_id`      varchar(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci  NULL DEFAULT NULL
+) ENGINE = MyISAM
+  CHARACTER SET = utf8
+  COLLATE = utf8_unicode_ci
+  ROW_FORMAT = DYNAMIC;
 
 SET FOREIGN_KEY_CHECKS = 1;
